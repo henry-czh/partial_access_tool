13bcb599ba25b2bbffb8b9d4926f557b906038ce
henry-czh <chaozhanghu123@qq.com>
2019-02-15 14:25:05 +0800

    v1.1
    modify some bugs

--- partial_access.py
+++ partial_access.py
@@ -10,61 +10,87 @@
 '''
 import sys
 
+
 def locat(locat,value,level,top):
-    locat=int(locat)
     level=int(level)
     top=int(top)
     unuse='0'*(len(value))
     if level==1:
-        if locat==0:
+        if locat=='00':
             be=unuse*top+unuse*top+unuse*top+value
-        elif locat==1:
+        elif locat=='01':
             be=unuse*top+unuse*top+value+unuse
-        elif locat==2:
+        elif locat=='10':
             be=unuse*top+value+unuse+unuse
-        elif locat==3:
+        elif locat=='11':
             be=value+unuse+unuse+unuse
     elif level==2:
-        if locat==0:
+        if locat=='0':
             be=unuse*top+value
-        elif locat==1:
+        elif locat=='1':
             be=value+unuse
     elif level==3:
         be=value
     return be
 
 def genbe(size):
-    addr_low=addr[-2:]
-
-    if int(addr_low[-1])<4:
-        byte_locate=0
-    elif 3<int(addr_low[-1])<8:
-        byte_locate=1
-    elif 7<int(addr_low[-1])<12:
-        byte_locate=2
-    elif 11<int(addr_low[-1])<16:
-        byte_locate=3
     
-    size=size.split('x')[1]
     if size=='2':
         base_be='f'
-        print 'BE  : 64\'h'+locat(addr_low[-2],locat(byte_locate,base_be,1,1),1,0)
-        print 'DATA: 512\'h'+locat(addr_low[-2],locat(byte_locate,data,1,1),1,0)
+        print 'BE  : 64\'h'+locat(addr[-6:-4],locat(addr[-4:-2],base_be,1,1),1,0)
+        print 'DATA: 512\'h'+locat(addr[-6:-4],locat(addr[-4:-2],data,1,1),1,0)
     elif size=='3':
         base_be='ff'
-        print 'BE  : 64\'h'+locat(addr_low[-2],locat(byte_locate,base_be,2,1),1,0)
-        print 'DATA: 512\'h'+locat(addr_low[-2],locat(byte_locate,data,2,1),1,0)
+        print 'BE  : 64\'h'+locat(addr[-6:-4],locat(addr[-4],base_be,2,1),1,0)
+        print 'DATA: 512\'h'+locat(addr[-6:-4],locat(addr[-4],data,2,1),1,0)
     elif size=='4':
         base_be='ffff'
-        print 'BE  : 64\'h'+locat(addr_low[-2],base_be,1,0)
-        print 'DATA: 512\'h'+locat(addr_low[-2],data,1,0)
+        print 'BE  : 64\'h'+locat(addr[-6:-4],base_be,1,0)
+        print 'DATA: 512\'h'+locat(addr[-6:-4],data,1,0)
     elif size=='5':
         base_be='ffffffff'
-        print 'BE  : 64\'h'+locat(addr_low[-3],base_be,2,0)
-        print 'DATA: 512\'h'+locat(addr_low[-3],data,2,0)
+        print 'BE  : 64\'h'+locat(addr[-6],base_be,2,0)
+        print 'DATA: 512\'h'+locat(addr[-6],data,2,0)
     elif size=='6':
         print 'BE  : 64\'h'+'ffffffff_ffffffff'
         print 'DATA: 512\'h'+data
+
+def getopts():
+    addr=''
+    for byte in addr_init:
+        if byte=='0':
+            addr=addr+'0000'
+        elif byte=='1':
+            addr=addr+'0001'
+        elif byte=='2':
+            addr=addr+'0010'
+        elif byte=='3':
+            addr=addr+'0011'
+        elif byte=='4':
+            addr=addr+'0100'
+        elif byte=='5':
+            addr=addr+'0101'
+        elif byte=='6':
+            addr=addr+'0110'
+        elif byte=='7':
+            addr=addr+'0111'
+        elif byte=='8':
+            addr=addr+'1000'
+        elif byte=='9':
+            addr=addr+'1001'
+        elif byte=='a':
+            addr=addr+'1010'
+        elif byte=='b':
+            addr=addr+'1011'
+        elif byte=='c':
+            addr=addr+'1100'
+        elif byte=='d':
+            addr=addr+'1101'
+        elif byte=='e':
+            addr=addr+'1110'
+        elif byte=='f':
+            addr=addr+'1111'
+    return addr
 
 if __name__=='__main__':
     #get opts
@@ -72,8 +98,9 @@ if __name__=='__main__':
         print 'usage:'
         print './partial_access.py [0xaddr] [0xsize] [0xdata]'
     else:
-        addr=sys.argv[1]
-        size=sys.argv[2]
+        addr_init=sys.argv[1].split('x')[1]
+        size=sys.argv[2].split('x')[1]
         data=sys.argv[3].split('x')[1]
+        addr=getopts()
         #main
         genbe(size)
